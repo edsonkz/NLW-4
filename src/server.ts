9cb1721ddfc38cc62b78eb0d0cc4856c8bdb40e3
Edson <edsonbisneto12@gmail.com>
2021-02-23 07:56:41 -0300

    Base do projeto criada. edson#2.

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express, { response } from 'express';
+
+const app = express();
+
+app.get('/', (req, res, next)=> {
+    return res.json({message:"Hello, World!"});
+})
+
+app.post('/', (req, res, next)=> {
+    return res.json({message:"Testando POST!"});
+})
+
+app.listen(3333)
